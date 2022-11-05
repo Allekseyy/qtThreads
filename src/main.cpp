928af852ec16e571e3d504bb93a989f76ef43411
Allekseyy <116344112+Allekseyy@users.noreply.github.com>
2022-11-05 09:47:53 +0400

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QThread>
+#include <QTimer>
+
+#include "Threads.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QTimer timer;
+    timer.setSingleShot(true);
+
+    QEventLoop loop;
+    ThreadContainer threads;
+
+    QObject::connect(&timer, SIGNAL(timeout()), &loop, SLOT(quit()));
+    QObject::connect(&threads, SIGNAL(ThreadsCompleted()), &loop, SLOT(exit()) );
+    timer.start(5000);
+    threads.StartThreads();
+    loop.exec();
+
+    qDebug() << "Ultimate answer: " << threads.GetString();
+    return 0;
+}
